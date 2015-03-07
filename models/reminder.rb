d847a406cc9b51cee1cd8b7ed0cdb80539e95aee
Jay Zheng <jay.j.zheng@gmail.com>
2015-03-06 23:58:05 -0800

    add associations

--- models/reminder.rb
+++ models/reminder.rb
@@ -4,6 +4,8 @@
 #############################
 module LeaveAtApi
   class Reminder < ActiveRecord::Base
+    belongs_to :user
+
     validates :origin, :destination, :arrival_time, :user_id, presence: true
     validates :is_finished, inclusion: { in: [true, false] }
 
--- models/user.rb
+++ models/user.rb
@@ -4,6 +4,9 @@
 #############################
 module LeaveAtApi
   class User < ActiveRecord::Base
+    has_many :reminders
+    has_many :sessions
+
     validates :email, :password, :password_confirmation, presence: true
     validates :is_admin, inclusion: { in: [true, false] }
   end
