17e152c073afbb16ff04291ffea3c82216016a9d
Jay Zheng <jay.j.zheng@gmail.com>
2015-03-07 22:04:52 -0800

    add null constraint on user_id for reminders

---
+++ db/migrate/20150308060342_change_column_user_id_on_reminders.rb
@@ -0,0 +1,9 @@
+class ChangeColumnUserIdOnReminders < ActiveRecord::Migration
+  def up
+    change_column_null :reminders, :user_id, false
+  end
+
+  def down
+    change_column_null :reminders, :user_id, true
+  end
+end
